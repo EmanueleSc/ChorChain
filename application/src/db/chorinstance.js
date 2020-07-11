65b5e2038788aad562de08510b1e43f4f2c03d6d
EmanueleSc <scalaemanuele92@gmail.com>
2020-07-11 12:34:45 +0200

    choreography instance db model

---
+++ application/src/db/chorinstance.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const chorinstanceSchema = mongoose.Schema({
+  idBpmnFile: String,
+  bpmnFileName: String,
+  startEvent: String,                               // e.g. 'Event_0tttznh'
+  roles: Object,                                    // e.g. { Customer: 'Org1MSP', Bike_center: 'Org2MSP', Insurer: 'Org3MSP' }
+  idChor: String,                                   // e.g. 'CHOR1'
+  contractName: String,                             // e.g. 'org.chorchain.choreographyprivatedata_1'
+  channel: String,                                  // e.g. 'channel123'
+  configTxProfile: { type: String, enum: ['TwoOrgsChannel', 'ThreeOrgsChannel'] },
+  contractVersion: Number,
+  created_at: { type: Date, required: true, default: Date.now }
+})
+
+module.exports = mongoose.model('chorinstance', chorinstanceSchema)
