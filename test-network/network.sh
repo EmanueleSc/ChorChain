fbc3df688c1d1269acc87215e9d68903c45a3783
EmanueleSc <scalaemanuele@gmail.com>
2020-10-28 10:09:41 +0100

    test commit new branch

--- test-network/network.sh
+++ test-network/network.sh
@@ -55,30 +55,6 @@ function printHelp() {
   echo "  network.sh deployCC -l javascript"
 }
 
-# Obtain CONTAINER_IDS and remove them
-# TODO Might want to make this optional - could clear other containers
-# This function is called when you bring a network down
-#function clearContainers() {
-#  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')
-#  if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
-#    echo "---- No containers available for deletion ----"
-#  else
-#    docker rm -f $CONTAINER_IDS
-#  fi
-#}
-
-# Delete any images that were generated as a part of this setup
-# specifically the following images are often left behind:
-# This function is called when you bring the network down
-#function removeUnwantedImages() {
-#  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*/) {print $3}')
-#  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
-#    echo "---- No images available for deletion ----"
-#  else
-#    docker rmi -f $DOCKER_IMAGE_IDS
-#  fi
-#}
-
 # Versions of fabric known not to work with the test network
 BLACKLISTED_VERSIONS="^1\.0\. ^1\.1\. ^1\.2\. ^1\.3\. ^1\.4\."
 
